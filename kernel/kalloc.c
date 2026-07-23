a07f3c7b21eb96c6b4b514a419f27ddb7723d597
Limgelir <3415502906@qq.com>
2026-07-23 16:10:51 +0800

    lock: optimize memory allocator

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,74 +9,170 @@
 #include "riscv.h"
 #include "defs.h"
 
+
 void freerange(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
-                   // defined by kernel.ld.
+
 
 struct run {
   struct run *next;
 };
 
-struct {
+
+// 每个 CPU 一个空闲链表
+struct kmem_cpu {
   struct spinlock lock;
   struct run *freelist;
-} kmem;
+};
+
+struct kmem_cpu kmems[NCPU];
+
+
 
 void
 kinit()
 {
-  initlock(&kmem.lock, "kmem");
+  for(int i = 0; i < NCPU; i++){
+    char name[16];
+
+    snprintf(name, sizeof(name), "kmem%d", i);
+
+    initlock(&kmems[i].lock, name);
+
+    kmems[i].freelist = 0;
+  }
+
   freerange(end, (void*)PHYSTOP);
 }
+
+
 
 void
 freerange(void *pa_start, void *pa_end)
 {
   char *p;
+
   p = (char*)PGROUNDUP((uint64)pa_start);
+
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
 }
 
-// Free the page of physical memory pointed at by pa,
-// which normally should have been returned by a
-// call to kalloc().  (The exception is when
-// initializing the allocator; see kinit above.)
+
+
+// Free one physical page.
 void
 kfree(void *pa)
 {
   struct run *r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+
+  if(((uint64)pa % PGSIZE) != 0 ||
+     (char*)pa < end ||
+     (uint64)pa >= PHYSTOP)
     panic("kfree");
+
 
   // Fill with junk to catch dangling refs.
   memset(pa, 1, PGSIZE);
 
+
   r = (struct run*)pa;
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+
+  push_off();
+
+  int id = cpuid();
+
+  acquire(&kmems[id].lock);
+
+  r->next = kmems[id].freelist;
+
+  kmems[id].freelist = r;
+
+  release(&kmems[id].lock);
+
+  pop_off();
 }
 
-// Allocate one 4096-byte page of physical memory.
-// Returns a pointer that the kernel can use.
-// Returns 0 if the memory cannot be allocated.
+
+
+// Allocate one 4096-byte page.
 void *
 kalloc(void)
 {
   struct run *r;
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+  push_off();
+
+  int id = cpuid();
+
+  pop_off();
+
+
+  /*
+   * 1. Try current CPU freelist
+   */
+
+  acquire(&kmems[id].lock);
+
+
+  r = kmems[id].freelist;
+
+
+  if(r){
+
+    kmems[id].freelist = r->next;
+
+    release(&kmems[id].lock);
+
+
+    memset((char*)r, 5, PGSIZE);
+
+    return (void*)r;
+  }
+
+
+  release(&kmems[id].lock);
+
+
+
+  /*
+   * 2. Steal from other CPU
+   */
+
+  for(int i = 0; i < NCPU; i++){
+
+    if(i == id)
+      continue;
+
+
+    acquire(&kmems[i].lock);
+
+
+    r = kmems[i].freelist;
+
+
+    if(r){
+
+      kmems[i].freelist = r->next;
+
+
+      release(&kmems[i].lock);
+
+
+      memset((char*)r, 5, PGSIZE);
+
+      return (void*)r;
+    }
+
+
+    release(&kmems[i].lock);
+  }
+
+
+
+  return 0;
 }
