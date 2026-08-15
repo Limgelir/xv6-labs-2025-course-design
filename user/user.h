10e11f2e7626ede183a84d286c0021b8d571c0f5
Limgelir <3415502906@qq.com>
2026-08-15 23:16:23 +0800

    fs: add symbolic links

--- user/user.h
+++ user/user.h
@@ -25,6 +25,7 @@ char* sys_sbrk(int,int);
 int pause(int);
 int uptime(void);
 
+int symlink(const char*, const char*);
 // ulib.c
 int stat(const char*, struct stat*);
 char* strcpy(char*, const char*);
